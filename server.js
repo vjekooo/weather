4c194ab254b0e7554bc1e957d51bcf9abde14764
vjekooo <vjeko.pavicin@gmail.com>
2017-03-17 20:45:57 +0100

    Http fixes

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ var express = require("express");
 
 // Create our app
 var app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+// app.use(function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] === 'http') {
+//         next();
+//     } else {
+//         res.redirect('http://' + req.hostname + reg.url);
+//     }
+// });
 
 app.use(express.static("public"));
 
-app.listen(port, function() {
-    console.log("Express server is up on port" + port);
+app.listen(PORT, function() {
+    console.log("Express server is up on port" + PORT);
 });
