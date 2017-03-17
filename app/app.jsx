f92981a2ffda40776f5e481b464bd70a6e27066e
vjekooo <vjeko.pavicin@gmail.com>
2017-03-17 15:58:08 +0100

    First steps

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+ReactDOM.render(
+    <Router history>
+
+    </Router>,
+    document.getElementById('app')
+);
